7fa12b2f47897eeecfd38576f5b8d28c71a65d37
fonsp <fonsvdplas@gmail.com>
2022-04-05 15:28:39 +0000

    Deploying to dist from  @ fe2d9c5684b398c8cc27479eb9c5914fe6fae641 🚀

---
+++ classic samples/Basic.jl
@@ -0,0 +1,47 @@
+### A Pluto.jl notebook ###
+# v0.14.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b2d786ec-7f73-11ea-1a0c-f38d7b6bbc1e
+md"""
+# The Basel problem
+
+_Leonard Euler_ proved in 1741 that the series
+
+```math
+\frac{1}{1} + \frac{1}{4} + \frac{1}{9} + \cdots
+```
+
+converges to
+
+```math
+\frac{\pi^2}{6}.
+```
+"""
+
+# ╔═╡ b2d79330-7f73-11ea-0d1c-a9aad1efaae1
+n = 1:100000
+
+# ╔═╡ b2d79376-7f73-11ea-2dce-cb9c449eece6
+seq = n .^ -2
+
+# ╔═╡ b2d792c2-7f73-11ea-0c65-a5042701e9f3
+sqrt(sum(seq) * 6.0)
+
+# ╔═╡ ec425e6e-6b9c-11eb-0c63-97fade58f6b5
+frontmatter = (
+    tags=["basic", "classic"],
+    author=[(
+        name="Pluto.jl",
+        url="https://github.com/JuliaPluto",
+    )],
+);
+
+# ╔═╡ Cell order:
+# ╟─b2d786ec-7f73-11ea-1a0c-f38d7b6bbc1e
+# ╠═b2d792c2-7f73-11ea-0c65-a5042701e9f3
+# ╠═b2d79330-7f73-11ea-0d1c-a9aad1efaae1
+# ╠═b2d79376-7f73-11ea-2dce-cb9c449eece6
+# ╟─ec425e6e-6b9c-11eb-0c63-97fade58f6b5
