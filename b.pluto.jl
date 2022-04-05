5a3f93621c445dcd094dab40e3bb9baf45ea9687
fonsp <fonsvdplas@gmail.com>
2022-04-05 15:14:01 +0000

    Deploying to dist from  @ be33036ce0e338a83eff5ecd42cc0d85612b3415 🚀

--- b.pluto.jl
+++ b.pluto.jl
@@ -6,9 +6,14 @@ using InteractiveUtils
 
 # ╔═╡ ec425e6e-6b9c-11eb-0c63-97fade58f6b5
 frontmatter = (
-    title="Something cool",
-    description="Very fun stuff is happening!",
+    title="Emissions mitigation and carbon dioxide removal to minimize climate suffering",
+    description="This interactive article lets you– the reader– run MARGO, a simple climate model, to explore what it takes to avoid the catastrophic impacts of global warming. The code in this webpage is reactive, meaning that the graphs and numbers automatically update whenever you change the climate model's inputs.",
     tags=["fun", "cool"],
+    author=[(
+        name="JuliaPluto",
+        url="https://github.com/JuliaPluto",
+        image="https://github.com/JuliaPluto.png",
+    )],
 );
 
 # ╔═╡ ec425e6e-6b9c-11eb-0c63-97fade58f6b4
