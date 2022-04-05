be33036ce0e338a83eff5ecd42cc0d85612b3415
Fons van der Plas <fonsvdplas@gmail.com>
2022-04-05 17:11:12 +0200

    sdf

---
+++ a copy 4.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ec425e6e-6b9c-11eb-0c63-97fade58f6b5
+frontmatter = (
+    title="Something cool",
+    description="Very fun stuff is happening!",
+    tags=["fun", "cool"],
+    author=[(
+        name="Fons van der Plas",
+        url="https://github.com/fonsp",
+    )],
+);
+
+# ╔═╡ ec425e6e-6b9c-11eb-0c63-97fade58f6b4
+md"Hello!"
+
+# ╔═╡ Cell order:
+# ╠═ec425e6e-6b9c-11eb-0c63-97fade58f6b4
+# ╠═ec425e6e-6b9c-11eb-0c63-97fade58f6b5
