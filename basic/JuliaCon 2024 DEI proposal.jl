9d2fde928867e6f6c4c6fde742fa7e136da77019
fonsp <fonsvdplas@gmail.com>
2024-01-02 16:33:59 +0000

    Deploying to dist from @ JuliaPluto/featured@34632ad6db9e6c3a7428d4bafad10b47796a31f6 🚀

---
+++ basic/JuliaCon 2024 DEI proposal.jl
@@ -0,0 +1,50 @@
+### A Pluto.jl notebook ###
+# v0.19.36
+
+#> [frontmatter]
+#> image = "https://juliacon.org/assets/2021/img/world_2800.png"
+#> title = "7 January: let's write a proposal together!"
+#> tags = ["announcements", "juliacon"]
+#> description = "Announcement: presentation + office hours event for writing your JuliaCon 2024 proposal! Diversity, Equity, and Inclusion"
+#> 
+#>     [[frontmatter.author]]
+#>     name = "JuliaCon 2024"
+#>     url = "https://github.com/JuliaCon"
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0302e4e2-a98a-11ee-3561-07816151da8f
+md"""
+# Announcing *Diversity, Equity, and Inclusion* Proposal-A-Thon
+
+Hey everyone,
+
+The [JuliaCon 2024](https://juliacon.org/2024/) Diversity, Equity, and Inclusion (DEI) Committee is excited to announce a **presentation + office hours** event for writing your JuliaCon 2024 proposal! [The call for proposals is out right now](https://pretalx.com/juliacon2024/cfp), and we are looking for both talks and posters for JuliaCon 2024.
+
+**Sign-up here for a link to join:**
+[https://forms.gle/V7sD95iEvC3Q6RZ5A](https://forms.gle/V7sD95iEvC3Q6RZ5A)
+
+![](https://juliacon.org/assets/2021/img/world_2800.png)
+
+
+If you are curious about the process of writing and submitting a proposal (**you don’t need an idea yet!**), we encourage you to attend! The event will start with a presentation on the process, followed by office hours where you can ask individual questions. We welcome a range of questions, such as: Should I submit a talk or a poster? Is X a good proposal topic? Is my proposal missing anything? How are proposals reviewed?
+
+We will run the event at two different times in an attempt to accommodate as many time zones as possible. Both times will take place on **Sunday, January 7th** with one at:
+- **11AM EST / 4PM UTC / 5PM CET / 9:30 IST** _(US + Europe-run)_ and one at 
+- **10:30AM IST / 1PM CST / 4PM AEDT** _(Australia-run)_.
+
+The proposal-a-thon will take place on [GatherTown](https://www.gather.town/), a video calling software complete with a virtual conference room! You will be sent a link after signing up. Event organizers will be in the GatherTown space for two hours after event start time. Attendees are welcome to use the GatherTown space for as long as they like afterwards!
+
+This event is intended to help community members who see themselves as under-represented across one or more axes such as gender, race, sexuality, education and socioeconomic boundaries. We particularly welcome first time JuliaCon attendees! We are excited to co-run this event with support from **Julia Gender Inclusive** and the **Julia Community Outreach and Development Fund**.
+"""
+
+# ╔═╡ d740e923-b717-4b6a-97fe-68b2c1d50b7c
+md"""
+![image](https://github.com/fonsp/Pluto.jl/assets/6933510/e2c75a0a-615e-440a-b9f0-5ddfa1e49c5b)
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─0302e4e2-a98a-11ee-3561-07816151da8f
+# ╟─d740e923-b717-4b6a-97fe-68b2c1d50b7c
