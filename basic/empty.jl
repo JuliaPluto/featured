26fb587e07abfdc95388f89caff5a648cdea0b80
fonsp <fonsvdplas@gmail.com>
2024-02-08 17:43:43 +0000

    Deploying to dist from @ JuliaPluto/featured@8ab6520c0998ada83924d019da7cf9964a4ce583 🚀

---
+++ basic/empty.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.38
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2a2b1a9c-c6a9-11ee-0fb7-2746d3703649
+
+
+# ╔═╡ Cell order:
+# ╠═2a2b1a9c-c6a9-11ee-0fb7-2746d3703649
