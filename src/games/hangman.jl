b6a98195e2e82b5c03a1f8b7722ea9c41d5af11b
Michael Tiemann <github@michaeltiemann.net>
2024-07-13 14:26:34 +0200

    add hangman notebook idea

---
+++ src/games/hangman.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.19.43
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ cd922998-3fee-4586-8458-7c2fa4a4bce1
+md"""
+# TODO
+
+- which Julia drawing library could be added?
+"""
+
+# ╔═╡ 1690c572-4112-11ef-0781-f979a8a12dca
+md"""
+# Hangman
+
+Using `PlutoUI`, it's really easy to code up a simple game of Hangman.
+For your entertainment, I've already provided the game logic.
+But the hangman picture could really use your Julia drawing skills.
+Can you even think of ways to modify the game itself?
+
+But don't get hung up too much ;-)
+"""
+
+# ╔═╡ Cell order:
+# ╠═cd922998-3fee-4586-8458-7c2fa4a4bce1
+# ╠═1690c572-4112-11ef-0781-f979a8a12dca
