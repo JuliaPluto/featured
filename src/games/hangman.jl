6c1e76e0036b9a0a1d8dc0b3a990a1464bf14923
Michael Tiemann <github@michaeltiemann.net>
2024-12-23 22:42:05 +0100

    start adding Turtle drawing

--- src/games/hangman.jl
+++ src/games/hangman.jl
@@ -1,21 +1,26 @@
 ### A Pluto.jl notebook ###
-# v0.19.45
+# v0.20.4
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
+    #! format: off
     quote
         local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
+    #! format: on
 end
 
 # ╔═╡ 84c2083b-7bc1-4ba2-8da3-4f2da8fe8449
 using PlutoUI, HypertextLiteral
+
+# ╔═╡ 17ae9a1a-5a51-420f-ba64-d58d2ca51303
+using PlutoTurtles
 
 # ╔═╡ cd922998-3fee-4586-8458-7c2fa4a4bce1
 md"""
@@ -36,6 +41,8 @@ md"""
 - use a button to "commit" to a guess
 - add internal links
 - fix the game description to actually include line breaks
+- use the 1000 nouns gist 
+- howto only partially animate the turtle?
 """
 
 # ╔═╡ 1690c572-4112-11ef-0781-f979a8a12dca
@@ -115,6 +122,72 @@ md"""
 Hoosah, the game wo\rks. But the graphics suck. Let's change that!
 """
 
+# ╔═╡ 2359ccfc-ee20-47ba-8410-03d7f38b36a0
+function quartercircle!(t, size=1.4, steps=12)
+	for i=1:steps
+		forward!(t, size/2)
+		right!(t, 90/steps)
+		forward!(t, size/2)
+	end
+end
+
+# ╔═╡ 4239c5bf-2e94-40f6-9f51-ebb0e7190cd6
+turtle_drawing() do t
+	# Move to start
+	penup!(t)
+	backward!(t, 15)
+	left!(t, 90)
+	forward!(t,10)
+	right!(t, 90)
+	pendown!(t)
+	# A hill
+	quartercircle!(t, 0.6, 10)
+	quartercircle!(t, 0.6, 10)
+	penup!(t)
+	quartercircle!(t, 0.6, 10)
+	quartercircle!(t, 0.6, 10)
+	quartercircle!(t, 0.6, 10)
+	pendown!(t)
+	# Upright beam
+	left!(t, 90)
+	forward!(t, 12)
+	# and a beam across
+	penup!(t)
+	left!(t, 90)
+	forward!(t, 2)
+	pendown!(t)
+	right!(t, 180)
+	forward!(t, 2)
+	forward!(t, 9)
+	# a diagonal beam for support
+	penup!(t)
+	backward!(t, 9)
+	right!(t, 90)
+	forward!(t, 2)
+	left!(t, 135)
+	pendown!(t)
+	forward!(t, sqrt(2*2^2))
+	penup!(t)
+	right!(t, 45)
+	forward!(t, 9-2)
+	pendown!(t)
+	# a rope
+	right!(t, 90)
+	forward!(t, 2)
+	# a head
+	left!(t, 90)
+	quartercircle!(t, 0.25, 5)
+	quartercircle!(t, 0.25, 5)
+	quartercircle!(t, 0.25, 5)
+	quartercircle!(t, 0.25, 5)
+end
+
+# ╔═╡ 3f12e054-0be7-4f40-9a8e-388a56595f38
+sqrt(2*3^2)
+
+# ╔═╡ 5e22e11c-446e-497b-9000-23238970439d
+sqrt(18)
+
 # ╔═╡ 1a7cd867-c199-4f95-ba63-fc2b36a89401
 md"""
 # Hangman
@@ -160,12 +233,998 @@ Feature
 Bug
 Gamer
 Developer
+time
+way
+year
+work
+government
+day
+man
+world
+life
+part
+house
+course
+case
+system
+place
+end
+group
+company
+party
+information
+school
+fact
+money
+point
+example
+state
+business
+night
+area
+water
+thing
+family
+head
+hand
+order
+john
+side
+home
+development
+week
+power
+country
+council
+use
+service
+room
+market
+problem
+court
+lot
+war
+police
+interest
+car
+law
+road
+form
+face
+education
+policy
+research
+sort
+office
+body
+person
+health
+mother
+question
+period
+name
+book
+level
+child
+control
+society
+minister
+view
+door
+line
+community
+south
+city
+god
+father
+centre
+effect
+staff
+position
+kind
+job
+woman
+action
+management
+act
+process
+north
+age
+evidence
+idea
+west
+support
+moment
+sense
+report
+mind
+church
+morning
+death
+change
+industry
+land
+care
+century
+range
+table
+back
+trade
+history
+study
+street
+committee
+rate
+word
+food
+language
+experience
+result
+team
+other
+sir
+section
+programme
+air
+authority
+role
+reason
+price
+town
+class
+nature
+subject
+department
+union
+bank
+member
+value
+need
+east
+practice
+type
+paper
+date
+decision
+figure
+right
+wife
+president
+university
+friend
+club
+quality
+voice
+lord
+stage
+king
+situation
+light
+tax
+production
+march
+secretary
+art
+board
+may
+hospital
+month
+music
+cost
+field
+award
+issue
+bed
+project
+chapter
+girl
+game
+amount
+basis
+knowledge
+approach
+series
+love
+top
+news
+front
+future
+manager
+account
+computer
+security
+rest
+labour
+structure
+hair
+bill
+heart
+force
+attention
+movement
+success
+letter
+agreement
+capital
+analysis
+population
+environment
+performance
+model
+material
+theory
+growth
+fire
+chance
+boy
+relationship
+son
+sea
+record
+size
+property
+space
+term
+director
+plan
+behaviour
+treatment
+energy
+peter
+income
+cup
+scheme
+design
+response
+association
+choice
+pressure
+hall
+couple
+technology
+defence
+list
+chairman
+loss
+activity
+contract
+county
+wall
+paul
+difference
+army
+hotel
+sun
+product
+summer
+set
+village
+colour
+floor
+season
+unit
+park
+hour
+investment
+test
+garden
+husband
+employment
+style
+science
+look
+deal
+charge
+help
+economy
+new
+page
+risk
+advice
+event
+picture
+commission
+fish
+college
+oil
+doctor
+opportunity
+film
+conference
+operation
+application
+press
+extent
+addition
+station
+window
+shop
+access
+region
+doubt
+majority
+degree
+television
+blood
+statement
+sound
+election
+parliament
+site
+mark
+importance
+title
+species
+increase
+return
+concern
+public
+competition
+software
+glass
+lady
+answer
+earth
+daughter
+purpose
+responsibility
+leader
+river
+eye
+ability
+appeal
+opposition
+campaign
+respect
+task
+instance
+sale
+whole
+officer
+method
+division
+source
+piece
+pattern
+lack
+disease
+equipment
+surface
+oxford
+demand
+post
+mouth
+radio
+provision
+attempt
+sector
+firm
+status
+peace
+variety
+teacher
+show
+speaker
+baby
+arm
+base
+miss
+safety
+trouble
+culture
+direction
+context
+character
+box
+discussion
+past
+weight
+organisation
+start
+brother
+league
+condition
+machine
+argument
+sex
+budget
+english
+transport
+share
+mum
+cash
+principle
+exchange
+aid
+library
+version
+rule
+tea
+balance
+afternoon
+reference
+protection
+truth
+district
+turn
+smith
+review
+minute
+duty
+survey
+presence
+influence
+stone
+dog
+benefit
+collection
+executive
+speech
+function
+queen
+marriage
+stock
+failure
+kitchen
+student
+effort
+holiday
+career
+attack
+length
+horse
+progress
+plant
+visit
+relation
+ball
+memory
+bar
+opinion
+quarter
+impact
+scale
+race
+image
+trust
+justice
+edge
+gas
+railway
+expression
+advantage
+gold
+wood
+network
+text
+forest
+sister
+chair
+cause
+foot
+rise
+half
+winter
+corner
+insurance
+step
+damage
+credit
+pain
+possibility
+legislation
+strength
+speed
+crime
+hill
+debate
+will
+supply
+present
+confidence
+mary
+patient
+wind
+solution
+band
+museum
+farm
+pound
+henry
+match
+assessment
+message
+football
+animal
+skin
+scene
+article
+stuff
+introduction
+play
+administration
+fear
+dad
+proportion
+island
+contact
+japan
+claim
+kingdom
+video
+existence
+telephone
+move
+traffic
+distance
+relief
+cabinet
+unemployment
+reality
+target
+trial
+rock
+concept
+spirit
+accident
+organization
+construction
+coffee
+phone
+distribution
+train
+sight
+difficulty
+factor
+exercise
+weekend
+battle
+prison
+grant
+aircraft
+tree
+bridge
+strategy
+contrast
+communication
+background
+shape
+wine
+star
+hope
+selection
+detail
+user
+path
+client
+search
+master
+rain
+offer
+goal
+dinner
+freedom
+attitude
+while
+agency
+seat
+manner
+favour
+fig
+pair
+crisis
+smile
+prince
+danger
+call
+capacity
+output
+note
+procedure
+theatre
+tour
+recognition
+middle
+absence
+sentence
+package
+track
+card
+sign
+commitment
+player
+threat
+weather
+element
+conflict
+notice
+victory
+bottom
+finance
+fund
+violence
+file
+profit
+standard
+jack
+route
+china
+expenditure
+second
+discipline
+cell
+reaction
+castle
+congress
+individual
+lead
+consideration
+debt
+option
+payment
+exhibition
+reform
+emphasis
+spring
+audience
+feature
+touch
+estate
+assembly
+volume
+youth
+contribution
+curriculum
+appearance
+boat
+institute
+membership
+branch
+bus
+waste
+heat
+neck
+object
+captain
+driver
+challenge
+conversation
+occasion
+code
+crown
+birth
+silence
+literature
+faith
+hell
+entry
+transfer
+gentleman
+bag
+coal
+investigation
+leg
+belief
+total
+major
+document
+description
+murder
+aim
+manchester
+flight
+conclusion
+drug
+tradition
+pleasure
+connection
+owner
+treaty
+desire
+professor
+copy
+ministry
+acid
+palace
+address
+institution
+lunch
+generation
+partner
+engine
+newspaper
+cross
+reduction
+welfare
+definition
+key
+release
+vote
+examination
+judge
+atmosphere
+leadership
+sky
+breath
+creation
+row
+guide
+milk
+cover
+screen
+intention
+criticism
+jones
+silver
+customer
+journey
+explanation
+green
+measure
+brain
+significance
+phase
+injury
+run
+coast
+technique
+valley
+drink
+magazine
+potential
+drive
+revolution
+bishop
+settlement
+christ
+metal
+motion
+index
+adult
+inflation
+sport
+surprise
+pension
+factory
+tape
+flow
+iron
+trip
+lane
+pool
+independence
+hole
+flat
+content
+pay
+noise
+combination
+session
+appointment
+fashion
+consumer
+accommodation
+temperature
+religion
+author
+nation
+northern
+sample
+assistance
+interpretation
+aspect
+display
+shoulder
+agent
+gallery
+republic
+cancer
+proposal
+sequence
+simon
+ship
+interview
+vehicle
+democracy
+improvement
+involvement
+general
+enterprise
+van
+meal
+breakfast
+motor
+channel
+impression
+tone
+sheet
+pollution
+beauty
+square
+vision
+spot
+distinction
+brown
+crowd
+fuel
+desk
+sum
+decline
+revenue
+fall
+diet
+bedroom
+soil
+reader
+shock
+fruit
+behalf
+deputy
+roof
+nose
+steel
+artist
+graham
+plate
+song
+maintenance
+formation
+grass
+spokesman
+ice
+talk
+program
+link
+ring
+expert
+establishment
+plastic
+candidate
+rail
+passage
+joe
+parish
+ref
+emergency
+liability
+identity
+location
+framework
+strike
+countryside
+map
+lake
+household
+approval
+border
+bottle
+bird
+constitution
+autumn
+cat
+agriculture
+concentration
+guy
+dress
+victim
+mountain
+editor
+theme
+error
+loan
+stress
+recovery
+electricity
+recession
+wealth
+request
+comparison
+lewis
+white
+walk
+focus
+chief
+parent
+sleep
+mass
+jane
+bush
+foundation
+bath
+item
+lifespan
+lee
+publication
+decade
+beach
+sugar
+height
+charity
+writer
+panel
+struggle
+dream
+outcome
+efficiency
+offence
+resolution
+reputation
+specialist
+taylor
+pub
+co-operation
+port
+incident
+representation
+bread
+chain
+initiative
+clause
+resistance
+mistake
+worker
+advance
+empire
+notion
+mirror
+delivery
+chest
+licence
+frank
+average
+awareness
+travel
+expansion
+block
+alternative
+chancellor
+meat
+store
+self
+break
+drama
+corporation
+currency
+extension
+convention
+partnership
+skill
+furniture
+round
+regime
+inquiry
+rugby
+philosophy
+scope
+gate
+minority
+intelligence
+restaurant
+consequence
+mill
+golf
+retirement
+priority
+plane
+gun
+gap
+core
+uncle
+thatcher
+fun
+arrival
+snow
+no
+command
+abuse
+limit
+championship
 """;
 
 # ╔═╡ 7002f4f2-7b33-4a73-ab18-b97001282b0b
 begin
-	new_word
-	random_word = rand(collect(eachsplit(random_word_list)))
+	new_word;
+	random_word = rand(collect(eachsplit(random_word_list)));
 end
 
 # ╔═╡ 61ff91c4-4257-470a-99d9-7080f52c11ba
@@ -193,10 +1252,12 @@ Markdown.parse("Can you picture the scene?\\\n\\\n" * description)
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
 HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+PlutoTurtles = "67697473-756c-6b61-6172-6b407461726b"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 
 [compat]
 HypertextLiteral = "~0.9.5"
+PlutoTurtles = "~1.0.1"
 PlutoUI = "~0.7.59"
 """
 
@@ -225,6 +1286,16 @@ deps = ["FixedPointNumbers", "Random"]
 git-tree-sha1 = "b10d0b65641d57b8b4d5e234446582de5047050d"
 uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
 version = "0.11.5"
+
+[[Compat]]
+deps = ["TOML", "UUIDs"]
+git-tree-sha1 = "8ae8d32e09f0dcf42a36b90d4e17f5dd2e4c4215"
+uuid = "34da2185-b29b-5c13-b0c7-acf172513d20"
+version = "4.16.0"
+weakdeps = ["Dates", "LinearAlgebra"]
+
+    [Compat.extensions]
+    CompatLinearAlgebraExt = "LinearAlgebra"
 
 [[CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -352,6 +1423,12 @@ deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
 version = "1.10.0"
 
+[[PlutoTurtles]]
+deps = ["AbstractPlutoDingetjes", "Compat", "HypertextLiteral", "InteractiveUtils", "Markdown", "PlutoUI"]
+git-tree-sha1 = "467866275ad02b4abf63c80bad78ad4f92744473"
+uuid = "67697473-756c-6b61-6172-6b407461726b"
+version = "1.0.1"
+
 [[PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
 git-tree-sha1 = "ab55ee1510ad2af0ff674dbcced5e94921f867a9"
@@ -451,7 +1528,7 @@ version = "1.2.13+1"
 [[libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.8.0+1"
+version = "5.11.0+0"
 
 [[nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -480,18 +1557,23 @@ version = "17.4.0+2"
 # ╠═163b6b19-dbe9-4a46-bef8-0af32dbba4b5
 # ╠═adee442c-bba4-4664-b082-50bb13962925
 # ╟─732db7c4-2408-45b2-ab8e-12630f7bb465
-# ╟─b7c82f65-bdad-495a-bfbe-43cda1bad3b8
-# ╟─ba1886df-509a-4016-8a28-d43b4403c999
+# ╠═b7c82f65-bdad-495a-bfbe-43cda1bad3b8
+# ╠═ba1886df-509a-4016-8a28-d43b4403c999
 # ╠═b0d0b7e4-72c6-4b0a-bf6e-1fc933b90994
 # ╠═494015e3-0e4b-4a7b-bc8e-1376fb2e9b59
 # ╠═f8d381c4-cf2f-4bc3-a1cd-02addd4ddb0c
 # ╠═fbe1c11e-ec34-4301-a6f0-c5c5b6d78adc
 # ╠═4e918fc4-762a-4aa0-984c-86569e808af6
 # ╠═28e9f3e1-2634-405f-954e-8041b4350754
+# ╠═17ae9a1a-5a51-420f-ba64-d58d2ca51303
+# ╠═2359ccfc-ee20-47ba-8410-03d7f38b36a0
+# ╠═4239c5bf-2e94-40f6-9f51-ebb0e7190cd6
+# ╠═3f12e054-0be7-4f40-9a8e-388a56595f38
+# ╠═5e22e11c-446e-497b-9000-23238970439d
 # ╠═1a7cd867-c199-4f95-ba63-fc2b36a89401
 # ╠═c1591133-f1ce-4f12-b68a-7edd126f2d58
-# ╠═a47488dd-c702-4ab9-81b2-bae55f4fc762
-# ╠═d3c1f0c1-eab7-4bbd-94ad-d42e16a6dc3c
-# ╠═8486bd1f-7dce-4f7f-ad60-c43f6dc6fc43
+# ╟─a47488dd-c702-4ab9-81b2-bae55f4fc762
+# ╟─d3c1f0c1-eab7-4bbd-94ad-d42e16a6dc3c
+# ╟─8486bd1f-7dce-4f7f-ad60-c43f6dc6fc43
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
